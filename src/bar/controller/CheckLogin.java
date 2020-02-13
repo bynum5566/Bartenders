c4ae1958013d2946ac38e1ad311b5c9ad193d1a2
Chia-Yu Lin <bynum5566@gmail.com>
2020-02-13 16:50:45 +0800

    接上首頁，處理中

--- src/bar/controller/CheckLogin.java
+++ src/bar/controller/CheckLogin.java
@@ -47,7 +47,7 @@ public class CheckLogin {
 		}
 
 		if (errors != null && !errors.isEmpty()) {
-			return "LoginPage";
+			return "index";
 		}
 
 		m.addAttribute("account", account);
@@ -65,7 +65,7 @@ public class CheckLogin {
 		}
 
 		m.addAttribute("msg", "帳號或密碼不正確");
-		return "LoginPage";
+		return "index";
 	}
 
 	@RequestMapping(path = { "/CcheckLogin.controller" }, method = { RequestMethod.POST })
