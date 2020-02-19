f0b195fb637f5f73d2952cb3b79027c2df73257f
AndreSigma <eeit11103sigma@outlook.com>
2020-02-19 14:53:43 +0800

    [精選酒吧]功能完成，隨機選取資料庫中n個酒吧，n可自由調整，目前為5，(若 n>= 目前酒吧總數，則會全數列出)。 使用者功能表加入[所有酒吧]連結。

--- src/bar/controller/DisplayBarListController.java
+++ src/bar/controller/DisplayBarListController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import bar.model.CartService;
 import bar.model.Company;
+import java.util.Random;
 
 @Controller
 @SessionAttributes(names = { 
@@ -20,19 +21,7 @@ import bar.model.Company;
 @EnableTransactionManagement
 public class DisplayBarListController {
 
-	private int i = 0; /*測式用參數*/
 	private CartService cartService;	/*間接呼叫其他DAO*/
-	
-	//private OrdersService ordersService;
-	//private CompanyService companyService;
-	//private ProductDataService productService;
-	//private ProductDataDAO productDataDAO ;
-	//private UsersService userService;
-
-	public void printI(String s){	/*測試用列印函數*/
-		this.i++;
-		System.out.printf("【%s】【%d】\n",s,i);
-	}
 	
 	public DisplayBarListController(
 			CartService cartService) {
@@ -40,7 +29,7 @@ public class DisplayBarListController {
 	}
 
 	@RequestMapping(path = { "/DisplayBarList.controller" })
-	public String displayBarProductProcessAction(
+	public String DisplayBarListProcessAction(
 			@ModelAttribute(name = "account") 
 			String account, 
 			Model m) {
@@ -48,14 +37,100 @@ public class DisplayBarListController {
 		
 		List <Company>	listOfBar =  new ArrayList<>();		/*儲存所有酒吧的列表*/
 		
-		printI("1");//1
-		
 		listOfBar.addAll(cartService.selectAllBar());	/*回傳資料庫中所有的Bar*/
 
 		m.addAttribute("listOfBar", listOfBar);
 		m.addAttribute("account", account);
-		
+		m.addAttribute("title", "所有酒吧");
+
 		CartService.Pf("/DisplayBarList.controller，結束");
+		return "DisplayBarList";
+
+	}
+
+	@RequestMapping(path = { "/DisplayRandomBarList.controller" })
+	public String DisplayRandomBarListProcessAction(
+			@ModelAttribute(name = "account") 
+			String account, 
+			Model m) {
+		CartService.Pf("/DisplayBarList.controller_TestVer，開始");
+		
+		List <Company>	listOfBar =  new ArrayList<>();		/*儲存所有酒吧的列表*/
+		
+		
+		int randomNumber = 5;	/*要隨機選取幾個數字*/
+		int numbersOfBar = 0;	/*用來存總共有幾個Bar，初始化*/		
+		
+		listOfBar.addAll(cartService.selectAllBar());	/*回傳資料庫中所有的Bar*/
+		
+		numbersOfBar = listOfBar.size();	/*目前資料庫有幾個Bar*/
+		CartService.printValueTypeTime("numbersOfBar",numbersOfBar);	/*印出*/
+		
+		/*如果要隨機的數量比，大於等於，總共數量。不需要隨機*/
+		if( randomNumber >= numbersOfBar) {
+			System.out.println("不需要隨機，因為randomNumber >= numbersOfBar");
+			m.addAttribute("listOfBar", listOfBar);
+			m.addAttribute("account", account);
+			m.addAttribute("title", "精選酒吧");
+			return "DisplayBarList";
+		}
+		
+		System.out.println("需要隨機");
+		Random random;
+		
+		List <Company>	randomListOfBar =  new ArrayList<>();	/*儲存隨機酒吧的列表*/
+		randomListOfBar = cartService.selectAllBar();
+		randomListOfBar.size();
+		
+		
+		//===========
+		System.out.println("只是印");
+		int index = 0;
+		for(Company CompanyX : randomListOfBar) 
+		{
+			CartService.printValueTypeTime("CompanyX.getCompanyId()",CompanyX.getCompanyId() );
+			CartService.printValueTypeTime("index", index);
+			index++;
+		}
+		//===========
+
+		System.out.println("隨機選取開始");
+		int numbersToDelete = numbersOfBar - randomNumber;	/*要刪除掉幾個多餘的項目*/
+		
+		int size=0;
+		int randomIndex=0;
+		/* 0 ~ numbersOfBar-1 */	/*從這個範圍中選擇1個來刪除*/
+		for(int i = 1 ; i <= numbersToDelete ; i++) {		/*多餘幾個項目就跑幾次*/
+			size = randomListOfBar.size();					/*目前的大小*/
+			CartService.printValueTypeTime("i",i);			/*印出i*/
+			CartService.printValueTypeTime("size",size);	/*印出size*/
+			
+			random = new Random();
+			randomIndex = random.nextInt(size);	/*範圍:0~目前的大小-1*/		/*從範圍中選一個來刪除*/
+			CartService.printValueTypeTime("randomIndex",randomIndex);	/*印出randomIndex*/
+			
+			CartService.printValueTypeTime("CompanyId",randomListOfBar.get(randomIndex).getCompanyId());	/*顯示將要刪除的CompanyId*/
+			randomListOfBar.remove(randomIndex);	/*刪除掉*/
+
+		}
+		
+		//===========
+		
+		System.out.println("只是印");
+		index = 0;
+		for(Company CompanyX : randomListOfBar) 
+		{
+			CartService.printValueTypeTime("CompanyX.getCompanyId()",CompanyX.getCompanyId() );
+			CartService.printValueTypeTime("index", index);
+			index++;
+		}
+		//===========
+		
+		m.addAttribute("listOfBar", randomListOfBar);
+		m.addAttribute("account", account);
+		m.addAttribute("title", "精選酒吧");
+		
+		CartService.Pf("/DisplayBarList.controller_TestVer，結束");
 		return "DisplayBarList";
 
 	}
